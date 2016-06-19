8acd54460ebbce8f31c195dd3f8a20137a1a5a5a
tpstar <hanlee12@gmail.com>
2016-06-19 12:51:04 -0500

    add player profiles

--- lib/batting_leaders/cli.rb
+++ lib/batting_leaders/cli.rb
@@ -1,12 +1,31 @@
 class BattingLeaders::CLI
-
   def call
+    @sorted_leading_batters = BattingLeaders::Player.sorted_batters
     list_players
     menu
   end
 
   def list_players
-    BattingLeaders::Player.all
+    puts "List of top MLB players today for batting average:"
+    puts ""
+    puts "Select a number of players displayed (10, 20, 30, or 40)"
+    puts ""
+    display_list
+  end
+
+  def display_list
+    number_players = gets.strip.to_i
+    if number_players == 10 || number_players == 20 || number_players == 30 || number_players == 40
+      puts "   Player                      batting ave"
+      @sorted_leading_batters[0,number_players].each_with_index do |player, index|
+        printf("%-3s", "#{index + 1}:")
+        printf("%-27s", "#{player.name}")
+        puts "#{player.batting_ave}"
+      end
+    else
+      puts "invalid input, please type (10, 20, 30, or 40)"
+      display_list
+    end
   end
 
   def menu
@@ -14,21 +33,30 @@ class BattingLeaders::CLI
     while input != "exit"
       puts "Enter the number of the player you'd like more info on, type list to see the list again or type exit."
       input = gets.strip.downcase
-      case input
-      when "1"
-        puts "more info on player 1"
-      when "2"
-        puts "more infor on player 2"
-      when "list"
+      if (input.to_i > 0) && (input.to_i < 41)
+        player_url = @sorted_leading_batters[input.to_i - 1].url
+        player_profile(player_url)
+      elsif input == "list"
         list_players
-      when "exit"
+      elsif input == "exit"
         goodbye
       else
-        puts "invalid input, please type again (1-20, list, or exit)"
+        puts "invalid input, please type again (1 .. 40), list, or exit"
       end
     end
   end
 
+  def player_profile(player_url)
+    player = BattingLeaders::Player.find_player_by_url(player_url)
+    puts "name #{player.name}"
+    puts "batting ave #{player.batting_ave}"
+    puts "number and position: #{player.number_position}"
+    puts "team: #{player.team}"
+    puts "birth date: #{player.birth_date}"
+    puts "experience: #{player.experience}"
+    puts "college: #{player.college}"
+    puts "Ht/Wt: #{player.ht_wt}"
+  end
 
   def goodbye
     puts "Check again soon!"
--- lib/batting_leaders/player.rb
+++ lib/batting_leaders/player.rb
@@ -1,24 +1,77 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+
 class BattingLeaders::Player
-  attr_accessor :name, :batting_ave, :player_url
+  attr_accessor :name, :batting_ave, :url, :number_position, :team, :birth_date, :experience, :college, :ht_wt
   @@all = []
 
   def initialize(player_hash)
     @name = player_hash[:name]
-    @location = player_hash[:batting_ave]
-    @player_url = player_hash[:player_url]
+    @batting_ave = player_hash[:batting_ave]
+    @url = player_hash[:url]
     @@all << self
   end
 
   def self.all
-#    @@all
-    puts "List of top MLB current players for batting average:"
-    puts ""
-    #    puts "Select a number of players displayed (10, 20, 30, or 40)"
-    puts ""
-    puts "   Player                      batting ave"
-    puts "1. Daniel Murphy               .359"
-    puts "2. Xander Bogaerts             .358"
+    @@all
   end
-end
 
-# binding.pry
+  def self.batting_leaders
+    self.row_scraper("odd")
+    self.row_scraper("even")
+    self.all
+  end
+
+  def self.sorted_batters
+    self.batting_leaders.sort_by {|player| player.batting_ave}.reverse! #sort by attribute (batting_ave) of Player's instance
+  end
+
+  def self.row_scraper(oddEven)
+    oddEven =="odd" ? odd_or_even_row = "oddrow" : odd_or_even_row = "evenrow"
+    get_page = Nokogiri::HTML(open"http://espn.go.com/mlb/stats/batting/_/year/2016/seasontype/2")
+
+    get_page.css("tr.#{odd_or_even_row}").collect do |player|
+      player_attributes = {:name => player.css("a").text,
+                           :url => player.css("a").attribute("href").value,
+                           :batting_ave => player.css(".sortcell").text}
+      player = self.new(player_attributes)
+    end
+
+    def self.find_player_by_url(player_url)
+      @@all.detect{|player| player.url == player_url}
+    end
+
+    def doc
+      @doc ||= Nokogiri::HTML(open"#{@url}").css(".player-bio")
+    end
+
+    def number_position
+      @number_position ||= doc.css(".general-info .first").text
+    end
+
+    def team
+      @team ||= doc.css(".general-info .last").text
+    end
+
+    def birth_date
+      @birth_date ||= doc.css(".player-metadata li")[0].text.gsub("Birth Date", "")
+    end
+
+    def experience
+      @experience ||= doc.css(".player-metadata li")[2].text.gsub("Experience", "")
+    end
+
+    def college
+      @college ||= doc.css(".player-metadata li")[3].text.gsub("College", "")
+    end
+
+    def ht_wt
+      @ht_wt ||= doc.css(".player-metadata li")[4].text.gsub("Ht/Wt", "")
+    end
+
+  end
+
+#binding.pry
+end
